8712fdd1e8a3fd323123b2625eab932f511c0c7d
Preet-Jyotpreet <38120430+Preet-Jyotpreet@users.noreply.github.com>
2019-10-28 14:23:08 -0700

    Create gradecalc.hpp

---
+++ gradecalc.hpp
@@ -0,0 +1,13 @@
+#ifndef GRADECALC
+#define GRADECALC
+#include<iostream>
+#include "info.hpp"
+
+void gradeCalc(int* pointArr, int* weightArr, StudentInfo* student_info,
+     const unsigned int SCORESIZE, const unsigned int STUSIZE);
+
+
+
+
+#endif
+                 
